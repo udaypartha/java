c67fb037bc619363770f2620b329ea3b2e9830ff
uday parha <udayparthananda@gmail.com>
2025-04-02 20:01:05 +0530

    add new java file

---
+++ bag.java
@@ -0,0 +1,15 @@
+package java;
+public class bag {
+    public static void main(String [] args)
+    {
+        int x = 10;
+        int y = 12;
+        if(x+y<20){
+            System.out.println("sum is less then 20");
+            }
+            else{
+                System.out.println("sum is greater then 20");
+            }
+        }
+
+    }
