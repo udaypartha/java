029122b1fad05cc988898dbbdf333f29fbba8739
uday parha <udayparthananda@gmail.com>
2025-04-07 21:56:21 +0530

    adding

---
+++ world.java
@@ -0,0 +1,12 @@
+public class world{
+    public static void main(String args[]){
+        int i = 0;
+        System.out.println("odd number up to 10");
+        do{
+            System.out.println(i);
+            i = i + 3;
+        }
+        while(i<=10);
+
+    }
+}
